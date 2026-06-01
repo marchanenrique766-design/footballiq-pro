c4f66d41c61650ed1f89fb37933e96b258429dfc
marchanenrique766-design <marchanenrique766@gmail.com>
2026-05-31 20:07:54 -0500

    Update get-data.js

--- api/get-data.js
+++ api/get-data.js
@@ -1,12 +1,1 @@
-export default async function handler(req, res) {
-  const { team } = req.query;
-  const apiKey = "TU_API_KEY_AQUI"; 
-  
-  try {
-    const response = await fetch(`https://api.sportmonks.com/v3/football/teams/search/${team}?api_token=${apiKey}`);
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Error al conectar con Sportmonks" });
-  }
-}
+
